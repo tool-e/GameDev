debf25964560d0e29e00d5fda14f382b3d0decfa
briantran818 <briatran@pdx.edu>
2026-06-19 19:37:22 -0700

    Rng Platform spawn

--- new-game-project/Player/player.gd
+++ new-game-project/Player/player.gd
@@ -3,7 +3,7 @@ extends CharacterBody2D
 
 
 const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+const JUMP_VELOCITY = -1500
 
 
 @onready var anim = get_node("AnimationPlayer") 
--- new-game-project/vert_world.gd
+++ new-game-project/vert_world.gd
@@ -1,15 +1,17 @@
 extends Node2D
 
-var platform = preload("res://platform.tscn")
-var width
+@export var scene_to_instantiate: PackedScene
+var width = 500
 
 func _ready():
-	width =  get_viewport_rect().size.x
 	randomize()
-	var y = 0
+	var y = 0  
 	while y > -3000:
-		var new_platform = platform.instantiate()
-		new_platform.Transform2D.Vector2(randf_range(-width/2,width*2),y))
+		var new_platform = scene_to_instantiate.instantiate()
+		new_platform.position = Vector2(randf_range(-width/2,width*2),y)
 		add_child(new_platform)
-		y-= randf_range(10, 210)
+		y-= randf_range(10, 75)
+
 	pass
+	
+	
