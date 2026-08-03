a7f6881e95c7422c8a1ebd0fdf20447a0fb90bec
briantran818 <briatran@pdx.edu>
2026-08-02 20:17:03 -0700

    rng platform update

--- new-game-project/vert_world.gd
+++ new-game-project/vert_world.gd
@@ -1,20 +1,29 @@
 extends Node2D
 
 @export var scene_to_instantiate: PackedScene
-var width = 500
+var width = 400
 @onready var player: Node2D = $PLAYER
+var prev_position
 
 func _ready():
 	randomize()
 	var y = 0  
 	while y > -3000:
 		var new_platform = scene_to_instantiate.instantiate()
-		new_platform.position = Vector2(randf_range(-width/2,width*2),y)
-		var old_x = new_platform.position.x
-		if(new_platform.position.x - old_x > 50 && new_platform.position.x != old_x):
-			new_platform.position.x = old_x + 15
+		if prev_position == null:
+			new_platform.position = Vector2(randf_range(-width , width),y)
+			prev_position = new_platform.position
+		else:
+			var offset = randf_range(-150,150)
+			if prev_position.x + offset >= width:
+				prev_position.x -= offset
+			if prev_position.x - offset < -width:
+				prev_position.x += offset
+			else:
+				prev_position.x = prev_position.x + offset
+			new_platform.position = Vector2(prev_position.x ,y)
 		add_child(new_platform)
-		y-= randf_range(10, 60)
+		y-= randf_range(100,125)
 		
 	pass
 	
