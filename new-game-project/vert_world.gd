eb735d502515eb5119f139ae28801133095bcac3
briantran818 <briatran@pdx.edu>
2026-08-02 20:25:39 -0700

    platform update 2.0

--- new-game-project/vert_world.gd
+++ new-game-project/vert_world.gd
@@ -10,11 +10,12 @@ func _ready():
 	var y = 0  
 	while y > -3000:
 		var new_platform = scene_to_instantiate.instantiate()
+		var rngesus_plat = scene_to_instantiate.instantiate()
+		var offset = randf_range(-150,150)
 		if prev_position == null:
 			new_platform.position = Vector2(randf_range(-width , width),y)
 			prev_position = new_platform.position
 		else:
-			var offset = randf_range(-150,150)
 			if prev_position.x + offset >= width:
 				prev_position.x -= offset
 			if prev_position.x - offset < -width:
@@ -22,7 +23,9 @@ func _ready():
 			else:
 				prev_position.x = prev_position.x + offset
 			new_platform.position = Vector2(prev_position.x ,y)
+			rngesus_plat.position = Vector2(randf_range(-width , width), y + offset)
 		add_child(new_platform)
+		add_child(rngesus_plat)
 		y-= randf_range(100,125)
 		
 	pass
